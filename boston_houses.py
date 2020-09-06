81e09808143073146e2cd10716b683d244e6aab2
aschwoegl <67163129+aschwoegl@users.noreply.github.com>
2020-09-06 12:19:10 -0500

    Update boston_houses.py

--- boston_houses.py
+++ boston_houses.py
@@ -38,3 +38,18 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], y_test[x])
+
+    
+ """ Variables used for regression model are:
+ CRIM     per capita crime rate by town
+ ZN       proportion of residential land zoned for lots over 25,000 sq.ft.
+ INDUS    proportion of non-retail business acres per town
+ RM       average number of rooms per dwelling
+ AGE      proportion of owner-occupied units built prior to 1940
+ DIS      weighted distances to five Boston employment centres
+ RAD      index of accessibility to radial highways
+ TAX      full-value property-tax rate per $10,000
+ PTRATIO  pupil-teacher ratio by town
+ B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+ LSTAT    % lower status of the population
+ MEDV     Median value of owner-occupied homes in $1000's"""
