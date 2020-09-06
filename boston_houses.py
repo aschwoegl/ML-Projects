2031e5424d0d65ee1ed66ead8b9d9fcd20fc9ddd
aschwoegl <67163129+aschwoegl@users.noreply.github.com>
2020-09-06 12:14:32 -0500

    Update boston_houses.py

--- boston_houses.py
+++ boston_houses.py
@@ -1,3 +1,5 @@
+""" This Program creates a regression model to predict the housing prices in Boston """
+
 import pandas as pd
 import numpy as np
 import sklearn
@@ -19,6 +21,9 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x,y,
 linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train)
 acc = linear.score(x_test, y_test)
+
+"""print model accuracy"""
+
 print(acc)
 
 """ output regession model """
@@ -27,7 +32,7 @@ print("Co: \n", linear.coef_)
 print("Intercept \n", linear.intercept_)
 
 
-""" print prediction and actual"""
+""" print prediction and actual value in $10,000s"""
 
 predictions = linear.predict(x_test)
 
